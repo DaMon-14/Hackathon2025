40222afd56a310295e40559e4de39b3882ef2567
DaMon14 <david.monoran@gmail.com>
2025-10-19 12:15:03 +0300

    test

---
+++ Project/Assets/Scripts/test.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Playables;
+
+// A behaviour that is attached to a playable
+public class test : MonoBehaviour
+{
+    public float moveDistance = 0.1f;
+    void Update()
+    {
+        // Detect Space key press (once per press)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y + moveDistance,
+                transform.position.z
+            );
+        }
+    }
+}
