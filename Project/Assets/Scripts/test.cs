6e12209402f285d592c24be5a1762f2bcc2787a0
Daniel <pdapdvpd@gmail.com>
2025-11-07 09:58:07 +0200

    more small changes:)

--- Project/Assets/Scripts/test.cs
+++ Project/Assets/Scripts/test.cs
@@ -1,3 +1,4 @@
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
@@ -6,10 +7,13 @@ using UnityEngine.Playables;
 public class test : MonoBehaviour
 {
     public float moveDistance = 0.1f;
+    public GameObject square = new GameObject();
+    
     void Update()
     {
+        
         // Detect Space key press (once per press)
-        if ( Keyboard.current.sKey.isPressed)
+        if ( Keyboard.current.spaceKey.isPressed)
         {
              transform.position = new Vector3(
                 transform.position.x,
@@ -17,5 +21,24 @@ public class test : MonoBehaviour
                 transform.position.z
             );
         }
+
+        if (square.transform.position.y <= -3)
+        {
+            square.transform.position = new Vector3(transform.position.x,-3,transform.position.z);
+        }
+        else
+        {
+            square.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        if (square.transform.position.y>5)
+        {
+            square.transform.position = new Vector3(transform.position.x, 5, transform.position.z);
+        }
+        else
+        {
+            square.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        }
+
     }
 }
