0cc7f6a735041e84b8f7521edcabba515a8c561c
Mirug126 <gavananamirela@gmail.com>
2025-11-08 10:26:46 +0200

    muzica ninja

---
+++ Project/Assets/Scripts/Script_joc_rau/script_manageraudio_ninja.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class script_manageraudio_ninja : MonoBehaviour
+{
+    [Header("-------Sursa audio------")]
+    [SerializeField] AudioSource sursa_muzica;
+
+    [Header("-------Sursa audio------")]
+    [SerializeField] AudioSource sursa_fx;
+
+    [Header("-------Clip audio----")]
+    public AudioClip background;
+
+    [Header("-------gunoi gresit----")]
+    public AudioClip gresit;
+
+    [Header("-------gunoi corect----")]
+    public AudioClip corect;
+
+    private void Start()
+    {
+        sursa_muzica.clip = background;
+        sursa_muzica.Play();
+        sursa_fx.clip = corect;
+    }
+
+    public void fgresit()
+    {
+        sursa_fx.clip = gresit;
+        sursa_fx.Play();
+    }
+
+    public void fcorect()
+    {
+        sursa_fx.clip = corect;
+        sursa_fx.Play();
+    }
+}
