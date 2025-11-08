73420e8400ee463a96fda41d9eff0b20c1104106
Mirug126 <gavananamirela@gmail.com>
2025-11-08 09:57:29 +0200

    muzica pe rau

--- Project/Assets/Scripts/Script_joc_rau/script_manageraudio.cs
+++ Project/Assets/Scripts/Script_joc_rau/script_manageraudio.cs
@@ -5,14 +5,35 @@ public class script_manageraudio : MonoBehaviour
     [Header("-------Sursa audio------")]
     [SerializeField] AudioSource sursa_muzica;
 
+    [Header("-------Sursa audio------")]
+    [SerializeField] AudioSource sursa_fx;
+
     [Header("-------Clip audio----")]
     public AudioClip background;
 
+    [Header("-------gunoi gresit----")]
+    public AudioClip gresit;
+
+    [Header("-------gunoi corect----")]
+    public AudioClip corect;
 
     private void Start()
     {
         sursa_muzica.clip = background;
         sursa_muzica.Play();
+        sursa_fx.clip = corect;
+    }
+
+    public void fgresit()
+    {
+        sursa_fx.clip = gresit;
+        sursa_fx.Play();
+    }
+
+    public void fcorect()
+    {
+        sursa_fx.clip = corect;
+        sursa_fx.Play();
     }
 
 }
